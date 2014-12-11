bb851cea75f900f8af3cf0f239e9a2bf0d8de5ab
Ryota Arai <ryota-arai@cookpad.com>
2014-12-11 22:54:59 +0900

    bundle gem.

---
+++ lib/ruboty/github_assignor.rb
@@ -0,0 +1,7 @@
+require "ruboty/github_assignor/version"
+
+module Ruboty
+  module GithubAssignor
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/github_assignor/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module GithubAssignor
+    VERSION = "0.0.1"
+  end
+end
