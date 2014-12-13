3be81326d0086bb6da132a9fd999bb9019418f59
Ryota Arai <ryota.arai@gmail.com>
2014-12-13 19:00:57 +0900

    Detail logs.

--- lib/ruboty/github_assignor/repo_watcher.rb
+++ lib/ruboty/github_assignor/repo_watcher.rb
@@ -16,21 +16,28 @@ module Ruboty
       end
 
       def check_issues(assign = true)
+        log "Checking new issues..."
+
         @octokit.issues(@repo).each do |issue|
           unless @checked_issue_ids.include?(issue[:id])
             log "New issue found (#{issue[:id]} / #{issue[:title]})"
             if assign && !issue[:assignee]
               # assign this issue
               assignee = @assignor.next
-              log "Assign this issue to #{assignee}"
+              log "Assigning this issue to #{assignee}..."
 
+              log "Updating assignee of GitHub issue..."
               @octokit.update_issue(@repo, issue[:number], assignee: assignee.github_name)
+
+              log "Reminding the user of the issue..."
               say(<<-EOC)
 @#{assignee.chat_name} さん、お願いします！
 
 #{issue[:title]}
 <#{issue[:html_url]}>
               EOC
+
+              log "Done."
             end
             @checked_issue_ids << issue[:id]
           end
@@ -56,6 +63,7 @@ module Ruboty
                  nil
                end
 
+        log "#{@from} -> #{@to}"
         @robot.say(
           body: body,
           from: from,
